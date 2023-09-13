866d177ab7afe5fa175ec92a189e4dd906069915
Eduardo Alves Araújo Silva <46291995867@info.local>
2023-09-13 10:05:12 -0300

    Exemplo de Data Base First CRUD de Estudio feito

---
+++ webApi.inlock.db/Controllers/EstudioController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApi.inlock.db.Domains;
+using webApi.inlock.db.Interfaces;
+using webApi.inlock.db.Repositories;
+
+namespace webApi.inlock.db.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class EstudioController : ControllerBase
+    {
+        private IEstudioRepository _estudioRepository { get; set; }
+
+        public EstudioController()
+        {
+            _estudioRepository = new EstudioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_estudioRepository.Listar());
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        [HttpGet("ListJogos")]
+        public IActionResult GetGames()
+        {
+            try
+            {
+                return Ok(_estudioRepository.ListarComJogos());
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                return Ok(_estudioRepository.BuscarPorId(id));
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _estudioRepository.Deletar(id);
+                return NoContent();
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Put(Estudio estudio)
+        {
+            try
+            {
+                _estudioRepository.Cadastrar(estudio);
+                return StatusCode(201);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+
+        public IActionResult Put(Guid id, Estudio estudio)
+        {
+            try
+            {
+                _estudioRepository.Atualizar(id, estudio);
+                return NoContent();
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+    }
+}
---
+++ webApi.inlock.db/Repositories/EstudioRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApi.inlock.db.Contexts;
+using webApi.inlock.db.Domains;
+using webApi.inlock.db.Interfaces;
+
+namespace webApi.inlock.db.Repositories
+{
+    public class EstudioRepository : IEstudioRepository
+    {
+        InlockContext ctx = new InlockContext();
+        public void Atualizar(Guid id, Estudio estudio)
+        {
+            Estudio busca = ctx.Estudios.Find(id);
+
+            if (busca != null)
+            {
+                busca.Nome = estudio.Nome;
+
+            }
+
+            ctx.Estudios.Update(busca);
+
+            ctx.SaveChanges();
+        }
+
+        public Estudio BuscarPorId(Guid id)
+        {
+            return ctx.Estudios.FirstOrDefault(e => e.IdEstudio == id);
+        }
+
+        public void Cadastrar(Estudio estudio)
+        {
+            estudio.IdEstudio = Guid.NewGuid();
+            ctx.Estudios.Add(estudio);
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(Guid id)
+        {
+            Estudio estudioBuscado = ctx.Estudios.Find(id);
+            ctx.Estudios.Remove(estudioBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Estudio> Listar()
+        {
+            return ctx.Estudios.ToList();
+        }
+
+        public List<Estudio> ListarComJogos()
+        {
+            return ctx.Estudios.Include(e => e.Jogos).ToList();
+        }
+    }
+}
